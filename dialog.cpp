6d9f5335823a1b8931547750df021580ec82ff05
tsvigo <11_70@bk.ru>
2024-02-07 06:58:58 +0300

    1

---
+++ dialog.cpp
@@ -0,0 +1,17 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// 1. изменение синапсов 2
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
